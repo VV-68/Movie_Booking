75c2614d1d8f5b09403630ea58b78b840cc72861
VV-68 <visakhvinod2004@gmail.com>
2026-03-07 19:09:32 +0530

    1

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: 'https://movie-booking-ivvj.onrender.com',
+    baseURL: 'https://movie-booking-ivvj.onrender.com/api',
 });
 
 API.interceptors.request.use((config) => {
